a7acfa7a9d300283209cf13eca1b33bd341bb21e
jtompkins84 <joseph.tompkins@mavs.uta.edu>
2016-02-11 00:28:44 -0600

    - structural changes to Sprite class - Sprite decendents must use one of the super constructors in their constructor - removed the 'image' & 'hitBox' fields - Sprite class constructors try to handle different combinations of frames and hit-boxes now - All frames & hit-boxes are stored in private arrays of  the Sprite class.

--- app/src/main/java/com/spaceinvaders/Sprite.java
+++ app/src/main/java/com/spaceinvaders/Sprite.java
@@ -11,33 +11,26 @@ import android.util.Log;
  */
 public abstract class Sprite {
 
-    // the sprite's associated image
-    protected SpriteImage image;
-    protected float resolutionRatio = 1.0f;
-
     /**
      * Array of images to animate. Should be in sequential order of frames.
      */
-    protected SpriteImage[] animation = null;
+    private SpriteImage[] frames = null;
+    // index of the current frame of animation
+    private int currFrame = 0;
+    // A ratio that represents the amount of <DPI of device> / <DPI of original image>.
+    //
+    private float resolutionRatio = 1.0f;
 
-    //TODO make setter method for animDelay. sets values less than 0 equal to 0.
-    /**
-     * Delay multiplier for number of game frames to skip before next animation frame update. Must
-     * be >= 0.
-     */
-    private short animDelay = 0;
 
     /**
      * (x, y) makes the top left coordinate of the sprite
      */
-    protected float x, y;
     // The "position" of the Sprite. Should never need to be manipulated directly. Sets itself
     // to the center of the hit-box.
     private PointF pos = new PointF();
 
     // player hit-box
-    protected RectF hitBox = null;
-    protected RectF[] hitBoxes = null;
+    private RectF[] hitBoxes = null;
 
     // pixel/second speed of the player
     protected float speed;
@@ -47,37 +40,75 @@ public abstract class Sprite {
 
     private boolean isResizedForResolution = false;
 
-    /**
-     * TODO can likely get rid of this later
-     * Default empty constructor.
-     */
-    protected Sprite() {
-    }
-
     protected Sprite(SpriteImage image) {
-        this.image = image;
+        this.frames[currFrame] = image;
     }
 
-    protected Sprite(SpriteImage[] animation) {
-        this.animation = animation;
+    protected Sprite(SpriteImage[] frames) {
+        this.frames = frames;
     }
 
     /**
-     *
+     * Instantiates a <code>Sprite</code> with a single frame and a single hit-box.
+     * @param image <code>SpriteImage</code>
+     * @param hitBox <code>RectF</code>
      */
     protected Sprite(SpriteImage image, RectF hitBox) {
-        this.image = image;
-        this.hitBox = hitBox;
+        frames = new SpriteImage[1];
+        hitBoxes = new RectF[1];
+        this.frames[currFrame] = image;
+        this.hitBoxes[currFrame] = hitBox;
         if(hitBox != null)
-            initHitBox(this.hitBox);
+            initHitBox(this.hitBoxes[currFrame]);
     }
 
-    protected Sprite(SpriteImage[] animation, RectF[] hitBoxes) {
-        this.animation = animation;
-        image = animation[0];
+    /**
+     * Instantiates a <code>Sprite</code> with multiple frames of animation. <code>hitBoxes</code>
+     * must be an array of length less-than or equal to the length of <code>frames</code>. Each
+     * hit-box at an index of <code>hitBoxes</code> will correspond with the frame at the same
+     * index of <code>frames</code>. If <code>hitBoxes</code> is of a length less than
+     * <code>frames</code>, then all indices greater than <code>hitBoxes</code>'s length are
+     * set to <code>null</code>. This essentially means that any frames without hit-boxes
+     * associated with them do not interact with other games entities/sprites.
+     * @param frames <code>SpriteImage[]</code>
+     * @param hitBoxes <code>RectF[]</code>
+     */
+    protected Sprite(SpriteImage[] frames, RectF[] hitBoxes) {
+        this.frames = frames;
 
-        this.hitBoxes = hitBoxes;
-        if(hitBoxes != null) {
+        this.hitBoxes = new RectF[this.frames.length];
+
+        for(int i = 0; i < this.frames.length; i++) {
+            if(i < hitBoxes.length) this.hitBoxes[i] = hitBoxes[i];
+            else this.hitBoxes[i] = null;
+        }
+
+        // Initialize hit-boxes
+        if(this.hitBoxes != null) {
+            for(RectF hb : this.hitBoxes) {
+                initHitBox(hb);
+            }
+        }
+    }
+
+    /**
+     * Instantiates a <code>Sprite</code> with multiple frames of animation. The
+     * hit-box will correspond with all the frames of the <code>frames</code> array.
+     * @param frames <code>SpriteImage[]</code>
+     * @param hitBox <code>RectF</code>
+     */
+    protected Sprite(SpriteImage[] frames, RectF hitBox) {
+        this.frames = frames;
+
+        if(hitBox != null) {
+            this.hitBoxes = new RectF[this.frames.length];
+
+            // set all hit-boxes in the array equal to 'hitBox' argument
+            for(int i = 0; i < this.frames.length; i++) {
+                hitBoxes[i] = hitBox;
+            }
+
+            // Initialize hit-boxes
             for(RectF hb : this.hitBoxes) {
                 initHitBox(hb);
             }
@@ -92,26 +123,93 @@ public abstract class Sprite {
 * v v v v v v v v v v v v v v v v v v v v v v v v v
 *******************************************************/
 
-    public RectF getHitBox() {
-        return hitBox;
+    /**
+     *
+     * @return the <code>SpriteImage</code> associated with the current frame of animation.
+     */
+    public SpriteImage getCurrentFrameImage() {
+        return frames[currFrame];
     }
+
+    /**
+     *
+     * @return <code>int</code> representing the index of the current frame in <code>frames[]</code>.
+     */
+    public int getCurrFrame() {
+        return currFrame;
+    }
+
+    /**
+     *
+     * @param frame <code>int</code> - the index of the frame to select.
+     */
+    public void setCurrFrame(int frame) {
+        if(frame < 0 || frame >= this.frames.length){
+            Log.e("setCurrFrame(" + frame + ")", "Not a valid frame.");
+            return;
+        }
+
+        currFrame = frame;
+    }
+
+    /**
+     * Advances to the next frame of animation. If the current frame is indexed at the end of
+     * <code>frames</code>,
+     */
+    public void advanceFrames() {
+        if(currFrame < frames.length - 1) currFrame++;
+        else if(currFrame >= frames.length) currFrame = 0;
+    }
+
+    /**
+     * Returns the hit-box associated with the current frame of animation. If there is no
+     * hit-box associated with the frame,
+     * @return
+     */
+    public RectF getHitBox() {
+        if(hitBoxes == null) return null;
+        if(currFrame < hitBoxes.length && hitBoxes[currFrame] != null) {
+            return hitBoxes[currFrame];
+        }
+
+        return null;
+    }
+
 
     /**
      * @return x-coordinate relative to center of hit-box
      */
     public float getX() {
-        return pos.x + hitBox.left + (hitBox.right - hitBox.left / 2);
+        if(hitBoxes == null || hitBoxes[currFrame] == null) {
+            float ratio = frames[currFrame].getDPIRatio();
+            float width = frames[currFrame].getBitmap().getWidth();
+            return (pos.x + (width * ratio) / 2);
+        }
+
+        return pos.x + hitBoxes[currFrame].left
+                + (hitBoxes[currFrame].right - hitBoxes[currFrame].left / 2);
     }
 
     /**
      * @return y-coordinate relative to center of hit-box
      */
     public float getY() {
-        return pos.y + hitBox.top + (hitBox.bottom - hitBox.top / 2);
+        if(hitBoxes == null || hitBoxes[currFrame] == null) {
+            float ratio = frames[currFrame].getDPIRatio();
+            // TODO resolve after SpriteImage is fixed to use .getScaledWidth()
+//            float width = frames[currFrame].getBitmap().getScaledWidth();
+//            return (pos.x + (width * ratio) / 2);
+        }
+
+        return pos.y + hitBoxes[currFrame].top
+                + (hitBoxes[currFrame].bottom - hitBoxes[currFrame].top / 2);
     }
 
-    public float getLength() {
-        return hitBox.left - hitBox.right;
+    public float getHitBoxLength() {
+        if(hitBoxes == null || hitBoxes[currFrame] == null) {
+            return 0.0f;
+        }
+        return hitBoxes[currFrame].left - hitBoxes[currFrame].right;
     }
 
     public float getSpeed() {
@@ -136,27 +234,23 @@ public abstract class Sprite {
      * @param y
      */
     public void setPosition(float x, float y) {
-        if(hitBox != null) {
-            pos.set(x - hitBox.left - ((hitBox.right - hitBox.left) / 2),
-                    y - hitBox.top - ((hitBox.bottom - hitBox.top) / 2));
-            hitBox.offsetTo(pos.x + hitBox.left, pos.y + hitBox.top);
-            Log.v("hitBox MOVED", (hitBox.right - hitBox.left) + "x" + (hitBox.bottom - hitBox.top));
-        }
-        else {
-            pos.set(x - (image.getBitmap().getWidth() / 2), y - (image.getBitmap().getHeight() / 2));
-        }
-    }
+        if(hitBoxes != null || hitBoxes[currFrame] != null) {
+            pos.set(x - hitBoxes[currFrame].left - ((hitBoxes[currFrame].right - hitBoxes[currFrame].left) / 2),
+                    y - hitBoxes[currFrame].top - ((hitBoxes[currFrame].bottom - hitBoxes[currFrame].top) / 2));
+            hitBoxes[currFrame].offsetTo(pos.x + hitBoxes[currFrame].left, pos.y + hitBoxes[currFrame].top);
 
-    /**
-     * Set position relative to previous position.
-     * @param dx
-     * @param dy
-     */
-    protected void move(float dx, float dy) {
-        pos.set(pos.x + dx, pos.y + dy);
-        if(hitBox != null) {
-            hitBox.offset(dx, dy);
+            Log.v("hitBox MOVED",
+                    (hitBoxes[currFrame].right - hitBoxes[currFrame].left)
+                    + "x"
+                    + (hitBoxes[currFrame].bottom - hitBoxes[currFrame].top));
+
+            return;
         }
+
+        float ratio = frames[currFrame].getDPIRatio();
+        float width = frames[currFrame].getBitmap().getWidth();
+        pos.set(x - ((frames[currFrame].getBitmap().getWidth() * ratio) / 2),
+                y - ((frames[currFrame].getBitmap().getHeight() * ratio) / 2));
     }
 
 /******************************************************
@@ -179,22 +273,42 @@ public abstract class Sprite {
      * @param paint <code>Paint</code> object used to draw
      */
     public void draw(Canvas canvas, Paint paint, boolean showHitBox) {
-        canvas.drawBitmap(image.getBitmap(), pos.x, pos.y, paint);
+        canvas.drawBitmap(frames[currFrame].getBitmap(), pos.x, pos.y, paint);
 
-        if(hitBox != null && showHitBox == true) {
+        if(hitBoxes != null || hitBoxes[currFrame] != null && showHitBox == true) {
             int oldColor = paint.getColor();
             paint.setARGB(150, 255, 0, 0);
-            canvas.drawRect(hitBox, paint);
+            canvas.drawRect(hitBoxes[currFrame], paint);
             paint.setColor(oldColor);
         }
     }
 
     private void initHitBox(RectF hitBox) {
-        float DPICorrectionRatio = image.getDPIRatio();
+        if (hitBox == null) return;
+        float DPICorrectionRatio = frames[currFrame].getDPIRatio();
+//        DPICorrectionRatio = (float)frames[0].getBitmap().getScaledWidth(640) / (float)frames[0].getBitmap().getWidth(); // TODO remove line
+
+        float w = frames[0].getBitmap().getWidth();
+        float h = frames[0].getBitmap().getHeight();
 
         hitBox.left = hitBox.left * DPICorrectionRatio;
         hitBox.top = hitBox.top * DPICorrectionRatio;
         hitBox.right = hitBox.right * DPICorrectionRatio;
         hitBox.bottom = hitBox.bottom * DPICorrectionRatio;
+//        hitBox.right = hitBox.left + w;
+//        hitBox.bottom = hitBox.top + h;
+        Log.v("SPRITE DIMENSION", w + "x" + h); // TODO remove line
+    }
+
+    /**
+     * Set position relative to previous position.
+     * @param dx
+     * @param dy
+     */
+    protected void move(float dx, float dy) {
+        pos.set(pos.x + dx, pos.y + dy);
+        if(hitBoxes != null || hitBoxes[currFrame] != null) {
+            hitBoxes[currFrame].offset(dx, dy);
+        }
     }
 }
--- app/src/main/java/com/spaceinvaders/SpriteImage.java
+++ app/src/main/java/com/spaceinvaders/SpriteImage.java
@@ -1,6 +1,7 @@
 package com.spaceinvaders;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 /**
  * Created by Joseph on 2/6/2016.
@@ -8,17 +9,36 @@ import android.graphics.Bitmap;
 public class SpriteImage {
 
     private Bitmap bmp;
+    private Canvas canvas;
     private float DPIRatio;
 
+    /**
+     * Instantiates a <code>SpriteImage</code>.
+     * @param   bitmap <code>Bitmap</code> - the bitmap image associated with
+     * this <code>SpriteImage</code>
+     * @param   DPIRatioMetric <code>float</code> - ratio multiplier used to
+     * adjust the size & placement of bitmaps, coordinates, hit-boxes, etc.
+     */
     public SpriteImage(Bitmap bitmap, float DPIRatioMetric) {
         this.bmp = bitmap;
+        this.canvas = canvas;
         this.DPIRatio = DPIRatioMetric;
     }
 
+    /**
+     * <code><b><i>getBitmap</i></b></code><p>
+     * @return <code><b>Bitmap</b></code>
+     */
     public Bitmap getBitmap() {
         return bmp;
     }
 
+    /**
+     * This is the ratio multiplier used to
+     * adjust the size & placement of bitmaps, coordinates, hit-boxes, etc.
+     * <code><b><i>getDPIRatio</i></b></code><p>
+     * @return <code><b>float</b></code>
+     */
     public float getDPIRatio() {
         return DPIRatio;
     }
